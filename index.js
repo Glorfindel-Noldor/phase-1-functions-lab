98510db644f9d52e7f57c61445c1049b842f1f53
Bogar Martinez <bogar.cm@icloud.com>
2023-06-07 20:04:19 -0500

    fake uber

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+  return someValue < 42 ? 42 - someValue : someValue - 42;
+}
+function distanceFromHqInFeet(someValue) {
+  distanceFromHqInBlocks(someValue);
+  const feet = 264;
+  return distanceFromHqInBlocks(someValue) * feet;
+}
+function distanceTravelledInFeet(start, destination) {
+  const feet = 264;
+  const total = Math.abs(start - destination);
+  return total * feet;
+}
+function calculatesFarePrice(start, destination) {
+  let blocks = Math.abs(start - destination);
+  const feet = 264;
+  const feetLim = feet * blocks;
+  if (feetLim <= 399) {
+    return 0;
+  } else if (400 <= feetLim && feetLim <= 2000) {
+    return (feetLim - 400) * 0.02;
+  } else if (2000 <= feetLim && feetLim <= 2500){
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
